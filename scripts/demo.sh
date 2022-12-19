4a8b50bf68ada45aef33303ec7850132722b0460
Mostafa Elsayed <melsayed@sumerge.com>
2022-12-19 16:11:35 +0200

    Update demo.sh

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -4,5 +4,5 @@ cd ../
 
 pwd
 
-mqsipackagebar -a demo.bar -k simple-demo -y esb.framework.lib esb.framework.java.lib
-
+# mqsipackagebar -a demo.bar -k simple-demo -y esb.framework.lib esb.framework.java.lib
+ibmint package --input-path workspace/source --output-bar-file bars/demo.bar
